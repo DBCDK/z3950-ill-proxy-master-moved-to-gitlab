b75bb6cc4e2cc03c90112241d38531c0ba6c2512
thl <thl@dbc.dk>
2017-10-19 07:13:16 +0200

    war file is now added to payara image. Changed the way the rest service signature to receive all the necessary information with input. Added logback.

---
+++ src/main/java/dk/dbc/z3950IllProxy/JsonQuery.java
@@ -0,0 +1,90 @@
+package dk.dbc.z3950IllProxy;
+
+public class JsonQuery {
+    private String server;
+    private String port;
+    private String user;
+    private String group;
+    private String password;
+    private String data;
+    private String timeout;
+    private String encoding;
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonQuery{" +
+                "server='" + server + '\'' +
+                ", port='" + port + '\'' +
+                ", user='" + user + '\'' +
+                ", group='" + group + '\'' +
+                ", password='" + password + '\'' +
+                ", data='" + data + '\'' +
+                ", timeout='" + timeout + '\'' +
+                ", encoding='" + encoding + '\'' +
+                '}';
+    }
+}
--- src/main/java/dk/dbc/z3950IllProxy/Z3950Endpoint.java
+++ src/main/java/dk/dbc/z3950IllProxy/Z3950Endpoint.java
@@ -1,72 +1,80 @@
 package dk.dbc.z3950IllProxy;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.perf4j.StopWatch;
+import org.perf4j.log4j.Log4JStopWatch;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 import org.yaz4j.ConnectionExtended;
 import org.yaz4j.Package;
 import org.yaz4j.exception.Bib1Exception;
 import org.yaz4j.exception.ZoomException;
 
 import javax.ejb.Stateless;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 @Stateless
 @Path("")
 public class Z3950Endpoint {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Z3950Endpoint.class);
+    private static final XLogger LOGGER = XLoggerFactory.getXLogger(Z3950Endpoint.class);
+
+    private ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @GET
-    @Path("status")
+    @Path("howru")
     public Response getStatus() {
         return Response.ok().entity("{}").build();
     }
 
     @POST
-    @Path("ill/{bibliotek}")
+    @Path("ill")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response sendIll( @PathParam("bibliotek") String bibliotek,
-                             String jobInputStreamData) throws ZoomException {
+    public Response sendIll(String inputData) {
+        LOGGER.entry(inputData);
+        StopWatch stopWatch = new Log4JStopWatch("Z3950Endpoint.sendIll");
+        try {
+            JsonQuery jsonQuery = objectMapper.readValue(inputData, JsonQuery.class);
+            String targetRef = "[target ref missing]";
+            String returnDoc = "[return doc missing]";
 
-        String targetRef="[target ref missing]";
-        String returnDoc="[return doc missing]";
-        
-        try(  ConnectionExtended connection= new ConnectionExtended("z3950.dbc.dk:2106/danbib", 0)) {
+            String targetHost = jsonQuery.getServer() + ":" + jsonQuery.getPort();
+            LOGGER.info("targetHost: " + targetHost);
 
-            connection.option("implementationName","DBC z3950 ill");
-            connection.option("user","user");
-            connection.option("group","group");
-            connection.option("password","ItIsSecret");
-            connection.connect();
+            try (ConnectionExtended connection = new ConnectionExtended(targetHost, 0)) {
+                connection.option("implementationName", "DBC z3950 ill proxy");
+                connection.option("user", jsonQuery.getUser());
+                connection.option("group", jsonQuery.getGroup());
+                connection.option("password", jsonQuery.getPassword());
+                connection.connect();
 
-            Package ill = connection.getPackage("itemorder");
-            ill.option("doc", jobInputStreamData);
-            ill.send();
-
-
-        } catch( Bib1Exception e) {
-            return Response.ok().entity("{\n  ERROR1: \n  "+ bibliotek                     
-                            + "\n  " + e.getMessage()
-                            + "\n}\n").build();
-
-        } catch( ZoomException e) {
-            LOGGER.error( e.getMessage() , e);
-
-            return Response.ok().entity("{\n  ERROR2: \n  "+ bibliotek                     
-                            + "\n  " + targetRef
-                            + "\n  " + returnDoc
-                            + "\n}\n").build();
+                Package ill = connection.getPackage("itemorder");
+                ill.option("doc", jsonQuery.getData());
+                ill.send();
+            } catch (Bib1Exception e) {
+                LOGGER.catching(XLogger.Level.ERROR, e);
+                return Response.status(HttpURLConnection.HTTP_BAD_GATEWAY).type(MediaType.APPLICATION_JSON).entity("{errorMessage: " + e.getMessage() + "}").build();
+            } catch (ZoomException e) {
+                LOGGER.catching(XLogger.Level.ERROR, e);
+                return Response.status(HttpURLConnection.HTTP_BAD_GATEWAY).type(MediaType.APPLICATION_JSON).entity("{errorMessage: " + e.getMessage()
+                        + ", targetRef: " + targetRef
+                        + ", returnDoc: " + returnDoc
+                        + "}").build();
+            }
+            return Response.ok().entity("{inputData: " + inputData
+                    + ", targetRef: " + targetRef
+                    + ", returnDoc: " + returnDoc
+                    + "}").build();
+        } catch (IOException e) {
+            return Response.status(HttpURLConnection.HTTP_BAD_GATEWAY).type(MediaType.APPLICATION_JSON).entity("{error: " + e.getMessage() + "}").build();
+        } finally {
+            stopWatch.stop();
+            LOGGER.exit();
         }
-
-        return Response.ok().entity("{\n  "+ bibliotek
-                + "\n  " + jobInputStreamData
-                + "\n  " + targetRef                  
-                + "\n  " + returnDoc
-                + "\n}\n").build();
     }
 }
